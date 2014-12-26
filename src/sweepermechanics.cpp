9847cbf01228d11c9dfd6fa8b2d9185d2a6abf77
Vincent Wong <duperduper@ucla.edu>
2014-12-25 22:00:58 -0800

    Integrated engine into mechanics

--- src/sweepermechanics.cpp
+++ src/sweepermechanics.cpp
@@ -1,11 +1,11 @@
 #include "gameconfig.hpp"
 #include "sweepermechanics.hpp"
-#include "sweeperengine.hpp"
 
 bool SweeperMechanics::init()
 {
     m_board = NULL;
-    m_engine = NULL;
+    m_minefield = NULL;
+    m_numfield = NULL;
     reset();
     return true;
 }
@@ -29,6 +29,16 @@ int** SweeperMechanics::getBoard()
     return m_board;
 }
 
+bool** SweeperMechanics::getMinefield()
+{
+    return m_minefield;
+}
+
+int** SweeperMechanics::getNumfield()
+{
+    return m_numfield;
+}
+
 int SweeperMechanics::getStatus()
 {
     return m_status;
@@ -36,10 +46,63 @@ int SweeperMechanics::getStatus()
 
 sf::Vector2f SweeperMechanics::getBoardSize()
 {
-    return sf::Vector2f(m_engine->getX(), m_engine->getY());
+    return m_boardSize;
 }
 
 double SweeperMechanics::getElapsed()
 {
     return m_elapsed;
+}
+
+bool** SweeperMechanics::generateMinefield(double p, int w, int h)
+{
+    int pn = p * 100000;
+    bool** minefield = new bool*[h];
+    for (int i = 0; i < h; i++)
+    {
+        minefield[i] = new bool[w];
+        for (int j = 0; j < w; j++)
+        {
+            int r = rand() % 100000;
+            if (r < pn)
+            {
+                minefield[i][j] = true;
+            }
+        }
+    }
+    return minefield;
+}
+
+int** SweeperMechanics::generateNumfield(bool** minefield, int w, int h)
+{
+    int** numfield = new int*[h];
+    for (int i = 0; i < h; i++)
+    {
+        numfield[i] = new int[w];
+        for (int j = 0; j < w; j++)
+        {
+            if (i > 0)
+            {
+                numfield[i][j] = numfield[i][j] + minefield[i-1][j];
+                if (j > 0)
+                    numfield[i][j] = numfield[i][j] + minefield[i-1][j-1];
+                if (j < w - 1)
+                    numfield[i][j] = numfield[i][j] + minefield[i-1][j+1];
+            }
+            if (i < h - 1)
+            {
+                numfield[i][j] = numfield[i][j] + minefield[i+1][j];
+                if (j > 0)
+                    numfield[i][j] = numfield[i][j] + minefield[i+1][j-1];
+                if (j < w - 1)
+                    numfield[i][j] = numfield[i][j] + minefield[i+1][j+1];
+            }
+            if (j > 0)
+                numfield[i][j] = numfield[i][j] + minefield[i][j-1];
+            if (j < w - 1)
+                numfield[i][j] = numfield[i][j] + minefield[i][j+1];
+            numfield[i][j] = numfield[i][j] + minefield[i][j];
+        }
+    }
+    return numfield;
 }
--- src/sweepermechanics.hpp
+++ src/sweepermechanics.hpp
@@ -14,11 +14,18 @@ public:
     void start();
     void tick(const sf::Time &elapsed);
     int** getBoard();
+    bool** getMinefield();
+    int** getNumfield();
     sf::Vector2f getBoardSize();
     int getStatus();
     double getElapsed();
 private:
-    SweeperEngine* m_engine;
+    bool** generateMinefield(double p, int w, int h);
+    int** generateNumfield(bool** minefield, int w, int h);
+
+    sf::Vector2f m_boardSize;
+    bool** m_minefield;
+    int** m_numfield;
     int** m_board;
     int m_status;
     double m_elapsed;
